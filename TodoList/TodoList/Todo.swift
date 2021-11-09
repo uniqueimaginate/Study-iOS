f0a2e8a0fa761aa425951a2132b547b1ddb4ab1f
uniqueimaginate <uniqueimaginate@gmail.com>
2021-11-09 09:07:33 +0900

    add TodoList Project

---
+++ TodoList/TodoList/Todo.swift
@@ -0,0 +1,142 @@
+//
+//  Todo.swift
+//  TodoList
+//
+//  Created by joonwon lee on 2020/03/19.
+//  Copyright © 2020 com.joonwon. All rights reserved.
+//
+
+import UIKit
+
+
+// TODO: Codable과 Equatable 추가
+struct Todo: Codable, Equatable {
+    let id: Int
+    var isDone: Bool
+    var detail: String
+    var isToday: Bool
+    
+    mutating func update(isDone: Bool, detail: String, isToday: Bool) {
+        // TODO: update 로직 추가
+        self.isDone = isDone
+        self.detail = detail
+        self.isToday = isToday
+    }
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        // TODO: 동등 조건 추가
+        return lhs.id == rhs.id
+    }
+}
+
+class TodoManager {
+    
+    static let shared = TodoManager()
+    
+    static var lastId: Int = 0
+    
+    var todos: [Todo] = []
+    
+    func createTodo(detail: String, isToday: Bool) -> Todo {
+        // [x] TODO: create로직 추가
+        
+        let nextId = TodoManager.lastId + 1
+        TodoManager.lastId = nextId
+        
+        return Todo(id: nextId, isDone: false, detail: detail, isToday: isToday)
+    }
+    
+    func addTodo(_ todo: Todo) {
+        //TODO: add로직 추가
+        todos.append(todo)
+        saveTodo()
+    }
+    
+    func deleteTodo(_ todo: Todo) {
+        //TODO: delete 로직 추가
+        
+        // 퍼포먼스 이슈를 크게 손상시키는게 아닌 이상, 가독성이 좋은 코드가 더 좋지 않을까? 하는 고찰이 있음
+        
+        // 2개의 경우로 구현 할 수 있다.
+        todos = todos.filter{ existingTodo in
+            existingTodo.id != todo.id
+        }
+        
+        // 축약형
+//        todos = todos.filter { $0.id != todo.id }
+        
+        // 이 부분이 더 효율적으로 쓸수 있다.
+//        if let index = todos.firstIndex(of: todo){
+//            todos.remove(at: index)
+//        }
+        saveTodo()
+    }
+    
+    func updateTodo(_ todo: Todo) {
+        //TODO: updatee 로직 추가
+        guard let index = todos.firstIndex(of: todo) else { return }
+        todos[index].update(isDone: todo.isDone, detail: todo.detail, isToday: todo.isToday)
+        saveTodo()
+    }
+    
+    func saveTodo() {
+        Storage.store(todos, to: .documents, as: "todos.json")
+    }
+    
+    func retrieveTodo() {
+        todos = Storage.retrive("todos.json", from: .documents, as: [Todo].self) ?? []
+        
+        let lastId = todos.last?.id ?? 0
+        TodoManager.lastId = lastId
+    }
+}
+
+class TodoViewModel {
+    
+    enum Section: Int, CaseIterable {
+        case today
+        case upcoming
+        
+        var title: String {
+            switch self {
+            case .today: return "Today"
+            default: return "Upcoming"
+            }
+        }
+    }
+    
+    private let manager = TodoManager.shared
+    
+    var todos: [Todo] {
+        return manager.todos
+    }
+    
+    var todayTodos: [Todo] {
+        return todos.filter { $0.isToday == true }
+    }
+    
+    var upcomingTodos: [Todo] {
+        return todos.filter { $0.isToday == false }
+    }
+    
+    var numOfSection: Int {
+        return Section.allCases.count
+    }
+    
+    func addTodo(_ todo: Todo) {
+        manager.addTodo(todo)
+    }
+    
+    func deleteTodo(_ todo: Todo) {
+        manager.deleteTodo(todo)
+    }
+    
+    func updateTodo(_ todo: Todo) {
+        manager.updateTodo(todo)
+    }
+    
+    func loadTasks() {
+        manager.retrieveTodo()
+    }
+}
+
